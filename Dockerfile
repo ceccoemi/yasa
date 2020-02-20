99a711ea75f6384f7bea1f78cedfc9c4beb4d0f4
ceccoemi <cecchini.mle@gmail.com>
2020-02-20 17:28:29 +0100

    Update example execution

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,21 @@ RUN apt-get update --fix-missing
 
 RUN apt-get install -y g++ cmake libgtest-dev google-mock lcov libsqlite3-dev clang-tidy-9
 
-### Build and install Google Test and Google Mock ###
+# Build and install Google Test and Google Mock
 RUN cd /usr/src/googletest/googlemock && \
     cd build-aux && \
     cmake -j$(nproc) .. && \
     make -j$(nproc) && \
     make install
 
-WORKDIR /usr/local/src/yasa
+ARG YASA_ROOT_DIR=/usr/local/src/yasa
+
+WORKDIR ${YASA_ROOT_DIR}
 COPY . .
-RUN mkdir debug-build && \
-    cd debug-build && \
-    cmake \
+
+# Debug build with warnings, tests and code coverage
+WORKDIR ${YASA_ROOT_DIR}/debug-build
+RUN cmake \
         -j$(nproc) \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON /usr/lib/llvm-9/ \
         -DCMAKE_BUILD_TYPE=DEBUG .. && \
@@ -32,18 +35,14 @@ RUN mkdir debug-build && \
         '/usr/local/include/*' \
         '/usr/local/src/yasa/tests/*' \
         --quiet --output-file coverage.info && \
-    lcov --list coverage.info && \
-    cd .. && \
-    mkdir release-build && \
-    cd release-build && \
-    cmake -j$(nproc) -DCMAKE_BUILD_TYPE=Release .. && \
+    lcov --list coverage.info
+
+# Build the program that is launched when `docker run yasa` is executed
+WORKDIR ${YASA_ROOT_DIR}/release-build
+RUN cmake -j$(nproc) -DCMAKE_BUILD_TYPE=Release .. && \
     make -j$(nproc) yasa && \
     cd src && \
     make install && \
-    yasa --version && \
-    cd ../../example && \
-    yasa train -n neg -p pos && \
-    yasa classify -f neg1.txt && \
-    yasa classify -f pos1.txt
+    yasa --version
 
-ENTRYPOINT ["yasa"]
+WORKDIR ${YASA_ROOT_DIR}
--- src/main.cc
+++ src/main.cc
@@ -12,47 +12,58 @@
 
 namespace fs = std::experimental::filesystem;
 
+auto trainFunc = [](auto negDir, auto posDir) {
+  if (!negDir.empty()) {
+    SqliteHandler handler("yasa.db");
+    SqliteDictionary<SqliteHandler> dictionary(&handler);
+    Trainer trainer(&dictionary);
+    int counter = 0;
+    for (const auto& entry : fs::directory_iterator(negDir)) {
+      TextFileReader reader(entry.path());
+      std::string text(reader.read());
+      std::vector<std::string> words = extractWords(text);
+      trainer.train(words, Sentiment::negative);
+      ++counter;
+      if (counter % 10 == 0) {
+        std::cout << "Processed " << counter << " neg files\n";
+      }
+    }
+  }
+  if (!posDir.empty()) {
+    SqliteHandler handler("yasa.db");
+    SqliteDictionary<SqliteHandler> dictionary(&handler);
+    Trainer trainer(&dictionary);
+    int counter = 0;
+    for (const auto& entry : fs::directory_iterator(posDir)) {
+      TextFileReader reader(entry.path());
+      std::string text(reader.read());
+      std::vector<std::string> words = extractWords(text);
+      trainer.train(words, Sentiment::positive);
+      ++counter;
+      if (counter % 10 == 0) {
+        std::cout << "Processed " << counter << " pos files\n";
+      }
+    }
+  }
+  return "Training done.";
+};
+
+auto classifyFunc = [](auto fileName) {
+  SqliteHandler handler("yasa.db");
+  SqliteDictionary<SqliteHandler> dictionary(&handler);
+  Classifier classifier(&dictionary);
+  TextFileReader reader(fileName);
+  std::string text(reader.read());
+  std::vector<std::string> words = extractWords(text);
+  Sentiment sentiment = classifier.classify(words);
+  return sentiment == Sentiment::positive ? "Positive" : "Negative";
+};
+
 int main(int argc, const char* argv[]) {
-  ArgumentParser argumentParser(
-      argc, std::vector<std::string>(argv, argv + argc),
-      [](auto negDir, auto posDir) {
-        SqliteHandler handler("yasa.db");
-        SqliteDictionary<SqliteHandler> dictionary(&handler);
-        Trainer trainer(&dictionary);
-        int counter = 0;
-        for (const auto& entry : fs::directory_iterator(negDir)) {
-          TextFileReader reader(entry.path());
-          std::string text(reader.read());
-          std::vector<std::string> words = extractWords(text);
-          trainer.train(words, Sentiment::negative);
-          ++counter;
-          if (counter % 10 == 0) {
-            std::cout << "Processed " << counter << " neg files\n";
-          }
-        }
-        counter = 0;
-        for (const auto& entry : fs::directory_iterator(posDir)) {
-          TextFileReader reader(entry.path());
-          std::string text(reader.read());
-          std::vector<std::string> words = extractWords(text);
-          trainer.train(words, Sentiment::positive);
-          ++counter;
-          if (counter % 10 == 0) {
-            std::cout << "Processed " << counter << " pos files\n";
-          }
-        }
-        return "Training done.";
-      },
-      [](auto fileName) {
-        SqliteHandler handler("yasa.db");
-        SqliteDictionary<SqliteHandler> dictionary(&handler);
-        Classifier classifier(&dictionary);
-        TextFileReader reader(fileName);
-        std::string text(reader.read());
-        std::vector<std::string> words = extractWords(text);
-        Sentiment sentiment = classifier.classify(words);
-        return sentiment == Sentiment::positive ? "Positive" : "Negative";
-      });
+  std::cout << fs::current_path() << '\n';
+  ArgumentParser argumentParser(argc,
+                                std::vector<std::string>(argv, argv + argc),
+                                trainFunc, classifyFunc);
   std::cout << argumentParser.main() << '\n';
   return 0;
 }
